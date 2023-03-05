59e9f59c951ced1e50359bae0f17f39c79cdbfff
Kim Fransman <kim.fransman@daresay.co>
2023-03-05 17:57:00 +0100

    first version of leaderboard

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,30 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_KEY: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+type LeaderboardItem = {
+  id: number
+  average: number
+  beers: number
+  events: number
+  fines: number
+  fines_rank: number
+  points: number
+  points_array: number[]
+  rank: number
+  special_array: any[]
+  player: Player | null
+}
+
+type Player = {
+  full_name: string
+  id: number
+  avatar_url: string | null
+  strokes?: number
+}
