06f1e9893c6047b2d898486e8320066d1d4bb24e
Kim Fransman <kim.fransman@daresay.co>
2023-04-29 23:56:11 +0200

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import compress from 'astro-compress';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress(), react()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({ analytics: true }),
 });
