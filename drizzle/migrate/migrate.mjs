b467e51b8aa157586577df793724b5e000345a2f
Kim Fransman <kim.fransman@daresay.co>
2024-10-17 08:05:56 +0200

    Bring back dockerfile and my own run script

---
+++ drizzle/migrate/migrate.mjs
@@ -0,0 +1,14 @@
+// This file is used to migrate the database to the current version
+// It is run when the docker container starts
+import Database from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+
+
+const migrationsFolder = process.argv[2] ?? '../';
+
+const sqlite = new Database(process.env.DATABASE_URL);
+
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder });
