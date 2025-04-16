8cb84aee290314b366d4a88a98519d6ba3ce4dcf
Kim Fransman <kim.fransman@daresay.co>
2025-04-16 19:28:59 +0200

    Skip database migrations for now

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Migrating database..."
-npm run db:migrate & PID=$!
-# Wait for migration to finish
-wait $PID
+# echo "Migrating database..."
+# npm run db:migrate & PID=$!
+# # Wait for migration to finish
+# wait $PID
 
 echo "Starting production server..."
 HOST=0.0.0.0 node dist/server/entry.mjs & PID=$!
