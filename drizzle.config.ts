4ef84ee291b22559b92d1f81416b8877f64ca705
Kim Fransman <kim.fransman@daresay.co>
2025-04-17 23:04:39 +0200

    Create team events as well

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'drizzle-kit';
 
 const isVercel = process.env.VERCEL;
 
-console.log('IS PROD?', isVercel);
-console.log(process.env.DATABASE_URL);
-console.log(process.env.TURSO_AUTH_TOKEN);
-
 export default defineConfig({
   dialect: isVercel ? 'turso' : 'sqlite',
   schema: './src/db/schema/*',
--- src/utils/formDataExtractors.ts
+++ src/utils/formDataExtractors.ts
@@ -3,7 +3,7 @@ export function extractPlayers(formData: FormData | null) {
     return null;
   }
 
-  const players: Record<string, { id?: string; strokes?: string }> = {};
+  const players: Record<string, { id: string; strokes: string }> = {};
   for (const [key, value] of formData.entries()) {
     // Match keys like players[0][id] or players[0]['strokes']
     const match = key.match(/^players\[(\d+)\]\[(?:'|")?(\w+)(?:'|")?\]$/);
@@ -11,10 +11,34 @@ export function extractPlayers(formData: FormData | null) {
       const idx = match[1];
       const prop = match[2];
       players[idx] = players[idx] || {};
-      (players[idx] as Record<'id' | 'strokes', string | undefined>)[prop as 'id' | 'strokes'] =
+      (players[idx] as Record<'id' | 'strokes', string>)[prop as 'id' | 'strokes'] =
         value as string;
     }
   }
   // Convert to array and filter out incomplete entries
   return Object.values(players).filter((p) => p.id);
+}
+
+export function extractTeams(formData: FormData | null) {
+  if (!formData) {
+    return null;
+  }
+
+  const teams: Record<string, { strokes: string; players: string[] }> = {};
+  for (const [key, value] of formData.entries()) {
+    // Match keys like teams[0][strokes] or teams[0][players]
+    const match = key.match(/^teams\[(\d+)\]\[(?:'|")?(\w+)(?:'|")?\]$/);
+    if (match) {
+      const idx = match[1];
+      const prop = match[2];
+      teams[idx] = teams[idx] || {};
+      if (prop === 'players') {
+        teams[idx].players = (value as string).split(',').filter(Boolean);
+      } else if (prop === 'strokes') {
+        teams[idx].strokes = value as string;
+      }
+    }
+  }
+  // Convert to array and filter out incomplete entries
+  return Object.values(teams).filter((t) => t.players && t.players.length > 0);
 }
