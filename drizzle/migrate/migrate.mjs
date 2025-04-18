5f339516b0bdd1c26e7cd0cc48274347c2a67607
Kim Fransman <kim.fransman@daresay.co>
2025-04-18 19:55:00 +0200

    ran prettier lint

--- drizzle/migrate/migrate.mjs
+++ drizzle/migrate/migrate.mjs
@@ -4,7 +4,6 @@ import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
-
 const migrationsFolder = process.argv[2] ?? '../';
 
 const sqlite = new Database(process.env.DATABASE_URL);
